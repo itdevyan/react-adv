905069dd9da963a40d531eff19c927e15ef15268
itdevyan <itdevyan@gmail.com>
2023-07-24 15:18:26 -0400

    [Clase 64] Compound Component Pattern - Tercer Paso

--- src/02-component-patterns/components/ProductCard.tsx
+++ src/02-component-patterns/components/ProductCard.tsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { ReactElement, createContext, useContext } from "react";
 import styles from "../styles/styles.module.css";
 import noImage from "../assets/no-image.jpg";
 import { useProduct } from "../hooks/useProduct";
 
 interface Props {
+  // en caso de utilizar HOC, se debe poner opcional children
+  children?: ReactElement | ReactElement[];
   product: Product;
 }
 
@@ -13,25 +15,46 @@ interface Product {
   img?: string;
 }
 
-export const ProductImage = ({ img = "" }) => {
-  return (
-    <img className={styles.productImg} src={img ? img : noImage} alt={img} />
-  );
-};
-
-export const ProductTitle = ({ title }: { title: string }) => {
-  return <span className={styles.productDescription}>{title}</span>;
-};
-
-interface ProductButtonsProps {
-  counter: number;
+interface ProductoContextProps {
+  counter: Number;
   increaseBy: (value: number) => void;
+  product: Product;
 }
 
-export const ProductButtons = ({
-  counter,
-  increaseBy,
-}: ProductButtonsProps) => {
+const ProductContext = createContext({} as ProductoContextProps);
+const { Provider } = ProductContext;
+
+export const ProductImage = ({ img = "" }) => {
+  const { product } = useContext(ProductContext);
+
+  let imgToShow: string;
+  if (img) {
+    imgToShow = img;
+  } else if (product.img) {
+    imgToShow = product.img;
+  } else {
+    imgToShow = noImage;
+  }
+
+  return <img className={styles.productImg} src={imgToShow} alt={img} />;
+};
+
+export const ProductTitle = ({ title }: { title?: string }) => {
+  const { product } = useContext(ProductContext);
+  let titleToShow: string;
+  if (title) {
+    titleToShow = title;
+  } else if (product.title) {
+    titleToShow = product.title;
+  } else {
+    titleToShow = "Product";
+  }
+  return <span className={styles.productDescription}>{titleToShow}</span>;
+};
+
+export const ProductButtons = () => {
+  const { increaseBy, counter } = useContext(ProductContext);
+
   return (
     <div className={styles.buttonsContainer}>
       <button className={styles.buttonMinus} onClick={() => increaseBy(-1)}>
@@ -45,14 +68,28 @@ export const ProductButtons = ({
   );
 };
 
-export const ProductCard = ({ product: { title, img } }: Props) => {
+export const ProductCard = ({ children, product }: Props) => {
   const { counter, increaseBy } = useProduct();
 
   return (
-    <div className={styles.productCard}>
-      <ProductImage img={img} />
+    <Provider
+      value={{
+        counter,
+        increaseBy,
+        product,
+      }}
+    >
+      <div className={styles.productCard}>
+        {children}
+        {/* <ProductImage img={img} />
       <ProductTitle title={title} />
-      <ProductButtons counter={counter} increaseBy={increaseBy} />
-    </div>
+      <ProductButtons counter={counter} increaseBy={increaseBy} /> */}
+      </div>
+    </Provider>
   );
 };
+
+// Otra forma de exportar HOC
+ProductCard.Title = ProductTitle;
+ProductCard.Image = ProductImage;
+ProductCard.Buttons = ProductButtons;
